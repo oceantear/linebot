d5c8ebbcd688a489594d567416aef617b9a9ac43
oceantear <oceantear@gmail.com>
2020-07-02 17:56:10 +0800

    init commit

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+app=Flask(__name__)
+#Channel access token
+line_bot_api = LineBotApi('xwYcIrRNGmj7SKJGpl2DSe+GdJ6JEFQXdoBTaVGLkNGPVdTrSTBKeDDxH3CJzK2eTfgIHHq60evtHvhWF1ldXa2h5SKXyMQKEiSVnpDQuxhzC9lwPTqYaSV88lMmGqxolbQrKgOTBMqLO2yjfM71cQdB04t89/1O/w1cDnyilFU=')
+#channel secret
+handler = WebhookHandler('c07bdeb9cdabc6c14307c208d5dd7ba0')
+
+@app.route('/')
+def hello():
+    return 'hello !'
+
+@app.route('/test')
+def test():
+    return "test"
+
+@app.route('/callback', methods=['GET','POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    print("body: ", body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+    return "callback"
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))    
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=80,debug=True)
