23472c543baa40fabb29aa3b65a3df725edbc150
jimmyang <jimmy.yang@iisigroup.com>
2020-07-03 14:05:34 +0800

    update app

--- app.py
+++ app.py
@@ -44,6 +44,9 @@ def callback():
     return 'OK'
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+    print(msg)
+    msg = msg.encode('utf-8')
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))    
