4c5a03698930bc94cfc588c2f82bbbd9d0e833ed
oceantear <oceantear@gmail.com>
2020-07-10 11:56:05 +0800

    fix bug

--- app.py
+++ app.py
@@ -260,8 +260,8 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=content))
-    elif "Demo" in event.message.text.lower():
-        input = event.message.text.split("Demo")
+    elif "demo" in event.message.text.lower():
+        input = event.message.text.lower().split("demo")
         command = input[1]
         print('command :', command)
         if "address" in command:
