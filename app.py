7c09484d0652d9b4f2f779ffaf594b3db0f3983b
oceantear <oceantear@gmail.com>
2020-07-10 11:54:50 +0800

    fix bug

--- app.py
+++ app.py
@@ -260,7 +260,7 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=content))
-    elif "Demo" in event.message.text:
+    elif "Demo" in event.message.text.lower():
         input = event.message.text.split("Demo")
         command = input[1]
         print('command :', command)
